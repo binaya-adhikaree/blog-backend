f2417205f5ecc76cdb83f9ba27bcec000dbeb58d
KaiZen <adhikaribijaya2035@gmail.com>
2025-08-09 21:53:33 +0545

    ready to deploy

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
-    origin: [
-      "https://bloghora.netlify.app/",
-      "http://localhost:3000",
-      "http://localhost:3001",
-    ],
+    origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use(cors());
 
 const authRoutes = require("./routes/authRoute");
 const blogRoutes = require("./routes/blogRoutes");
